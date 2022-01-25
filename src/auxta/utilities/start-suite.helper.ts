fba0a81c5439903d359d753938e00e6a078be8e6
Slavi Traykov <st@auxcode.com>
2022-01-25 12:36:29 +0200

    testing and logs for aws

--- src/auxta/utilities/start-suite.helper.ts
+++ src/auxta/utilities/start-suite.helper.ts
@@ -2,15 +2,20 @@ import axios from 'axios';
 import { config } from "../configs/config";
 
 export async function startSuite(suites: string[], reportId?: string) {
-    console.log(reportId);
-    console.log(suites);
     if (suites.length === 0) return;
     const next = suites.shift();
     console.log('Starting next suite: ' + next);
     try{
-        await axios.post(
-            `${config.netlifyPath}.netlify/functions/${next}?token=${config.token}`,
-            {nextSuites: suites, reportId: reportId})
+        if (config.netlifyPath.includes('amazonaws')) {
+            await axios.post(
+                `${config.netlifyPath}${next}?token=${config.token}`,
+                {nextSuites: suites, reportId: reportId})
+        } else {
+            await axios.post(
+                `${config.netlifyPath}.netlify/functions/${next}?token=${config.token}`,
+                {nextSuites: suites, reportId: reportId})
+        }
+
     } catch (e){
         // skip the suite if not found
         await startSuite(suites, reportId);
