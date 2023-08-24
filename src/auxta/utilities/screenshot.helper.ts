d1b18628de4100adf441c48712f1cadd74717fc7
Slavi Traykov <st@auxcode.com>
2023-08-24 16:44:01 +0300

    screenshot.helper.ts update

--- src/auxta/utilities/screenshot.helper.ts
+++ src/auxta/utilities/screenshot.helper.ts
@@ -1,8 +1,11 @@
 import puppeteer  from "../../puppeteer/puppeteer";
+import log from "../services/log.service";
+import StatusOfStep from "../enums/status-of.step";
 
 export async function captureScreenshot() {
     try{
         const pages = await puppeteer.defaultPage.browser().pages();
+        console.log(pages.length);
         if (!pages.length) return undefined;
         const lastPage = pages[pages.length - 1] || puppeteer.defaultPage;
 
@@ -11,9 +14,15 @@ export async function captureScreenshot() {
                 encoding: 'binary'
             }
         );
+        console.log('screenshotBuffer');
+        console.log(screenshotBuffer);
         if (Buffer.isBuffer(screenshotBuffer))
             return screenshotBuffer;
-    } catch (e){
+    } catch (e:any){
+        const pages = await puppeteer.defaultPage.browser().pages();
+        console.log(pages.length);
+        console.log(e);
+        log.push('When', e.toString(), StatusOfStep.FAILED);
     }
     return undefined
 }
